d28d06bdfae9ece5e63c9bd0c1c2f44fdcb61611
Nikhil I J <nikhilij99@gmail.com>
2022-12-13 15:30:15 +0530

    Changes not staged for commit: 	deleted:    coo.html 	deleted:    cookies.js 	modified:   index.html
     Untracked files:
    	cookies.html

--- cookies.js
+++ cookies.js
@@ -1,21 +1,21 @@
-function ReadCookie() {
-    var allcookies = document.cookie;
-    document.write ("All Cookies : " + allcookies );
-    cookiearray = allcookies.split(';');
-    
-    
-    for(const element of cookiearray) {
-       name = element.split('=')[0];
-       value = element.split('=')[1];
-       document.write ("Key is : " + name + " and Value is : " + value);
-    }
+function getCookie(uname) {
+   let name = uname + "=";
+   let decodedCookie = decodeURIComponent(document.cookie);
+   let ca = decodedCookie.split(';');
+   for(const element of ca) {
+     let c = element;
+     while (c.charAt(0) == ' ') {
+       c = c.substring(1);
+     }
+     if (c.indexOf(name) == 0) {
+       return c.substring(name.length, c.length);
+     }
+   }
+   return "";
 }
-function setCookie() {
-   var now = new Time();
-   now.setTime(now.getTime() + (exdays * 24 * 60 * 60 * 1000));
-   cookievalue = escape(document.form.uname.value) + ";"
-   
-   document.cookie = "name=" + cookievalue;
-   document.cookie = "expires=" + now.toUTCString() + ";"
-   document.write ("Setting Cookies : " + "name=" + cookievalue );
-}
+function setCookie(uname,cvalue,exdays) {
+   const d = new Date();
+   d.setTime(d.getTime() + (exdays*24*60*60*1000));
+   let expires = "expires=" + d.toUTCString();
+   document.cookie = uname + "=" + cvalue + ";" + expires + ";path=/";
+ }
